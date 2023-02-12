761f965f543c8efd05cd4c8ebe6be1bf01a2ef87
paul47606 <paul47606@gmail.com>
2023-02-12 12:46:37 +0530

    we can update it

---
+++ password_generator.py
@@ -0,0 +1,29 @@
+import random
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "(){}[],;:-_!@#$%^&*"
+
+upper, lower, nums, syms, = True, True, True, True
+
+all =""
+
+if upper:
+    all = all + uppercase_letters
+
+if lower:
+    all = all + lowercase_letters
+
+if nums:
+    all = all + digits
+
+if symbols:
+    all = all + symbols
+
+length = 20
+amount = 100
+
+for x in range(amount):
+    password = "".join(random.sample(all, length))
+    print(password)
